8f5a08ac4031519237b0149593aa7f9d67408c16
lucasdolha <61910153+lucasdolha@users.noreply.github.com>
2020-05-31 19:36:46 +0300

    Initial commit

---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+//jshint esversion:6
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Footer()
+{
+  const date = new Date();
+  var year = date.getFullYear();
+  return (<footer><p>Copyright © {year}</p></footer>);
+}
+
+export default Footer;
